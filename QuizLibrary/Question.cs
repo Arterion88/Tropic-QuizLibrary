4f303100ae3f3617ec88ab6a7f16b58a14abf301
Arterion <david.holan93@gmail.com>
2018-08-27 15:53:28 +0200

    question re-work

--- QuizLibrary/Question.cs
+++ QuizLibrary/Question.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -45,7 +49,6 @@ namespace QuizLibrary
             return randomize ? ShuffleList<Question>(questions) : questions;
         }
 
-
         public static List<E> ShuffleList<E>(List<E> inputList)
         {
             List<E> randomList = new List<E>();
@@ -67,4 +70,119 @@ namespace QuizLibrary
             return questions.FindAll(x => x.TestAnswer == -1);
         }
     }
+
+    public struct Question2
+    {
+        public int Id { get; set; }
+
+        private readonly string testName;
+
+        public string Prompt { get; set; }
+
+        public string PromptImage { get { return String.Format("{0}.Images.{1}_0", testName, Id); } }
+
+        public int CorrectAnswer { get; set; }
+
+        public List<Answer> Answers { get; set; }
+
+        public Question2(int id,string testName, string question,int correctAnswer)
+        {
+            Id = id;
+            string[] items = question.Split(';');
+            Prompt = items[0];
+            CorrectAnswer = correctAnswer;
+            this.testName = testName;
+            Answers = new List<Answer>();
+            for (int i = 0; i < items.Length; i++)
+                Answers.Add(new Answer() { Text = items[i], Image= String.Format("{0}.Images.{1}_{2}", testName, Id,i+1) });
+        }                   
+
+        public struct Answer
+        {
+            public enum AnswerType
+            {
+                Button, Image, ImageButton, None
+            }
+
+            public AnswerType GetAnswerType
+            {
+                get
+                {
+                    AnswerType result;
+                    result = 
+                        (Text == "")
+                        ?
+                        (ImageExists) ? AnswerType.Image : AnswerType.None
+                        :
+                        (ImageExists) ? AnswerType.ImageButton : AnswerType.Button;
+                    return result;
+                }
+            }
+
+
+
+            public string Text { get; set; }
+            public string Image { get; set; }
+
+            public bool ImageExists {
+                get
+                {
+                    return false; //TODO:Implement Image Exist Check
+                }
+            }
+        }
+    }
+
+    public class Test
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Index is language code (cs,en...)
+        /// </summary>
+        public Dictionary<string, List<Question2>> Questions { get; set; }
+
+        public Test(string testName)
+        {
+
+            foreach (string fileName in GetAllTxt(testName))
+            {
+                if (fileName == "answers")
+                {
+                    //TODO: Load correct answers from file
+                    continue;
+                }
+                Console.WriteLine(fileName.Split('.')[3]);
+
+                Questions.Add(fileName.Split('.')[3], GetQuestionFromTxt(fileName));
+            }
+            Console.ReadKey();
+        }
+        
+        public List<Question2> GetQuestionFromTxt(string fileName)
+        {
+            List<Question2> questions = new List<Question2>();
+            int index =0;
+            var lines = File.ReadLines(fileName);
+            foreach (var line in lines)
+            {
+                questions.Add(new Question2(index,Name,line,0)); //TODO:Implement correct answer
+                index++;
+            }
+
+                return questions;
+        }
+
+        private string[] GetAllTxt(string testName)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            string folderName = string.Format("{0}.Tests.{1}", executingAssembly.GetName().Name, testName);
+            return executingAssembly
+                .GetManifestResourceNames()
+                .Where(r => r.StartsWith(folderName) && r.EndsWith(".txt"))
+                //.Select(r => r.Substring(constantResName.Length + 1))
+                .ToArray();
+        }
+    }
+
 }
